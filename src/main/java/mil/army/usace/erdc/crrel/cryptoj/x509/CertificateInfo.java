e5932d683fa3c89108463493495d94626316fa8b
charles-p-howe <charles.p.howe@usace.army.mil>
2025-04-28 11:15:23 -0400

    changes to use FIPS version of bouncy castle

--- src/main/java/mil/army/usace/erdc/crrel/cryptoj/x509/CertificateInfo.java
+++ src/main/java/mil/army/usace/erdc/crrel/cryptoj/x509/CertificateInfo.java
@@ -24,6 +24,7 @@ import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1String;
 import org.bouncycastle.asn1.ASN1TaggedObject;
+import org.bouncycastle.asn1.DLTaggedObject;
 import org.bouncycastle.asn1.DERIA5String;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERTaggedObject;
@@ -350,14 +351,25 @@ public class CertificateInfo {
         for (List item : altNames) {
             Integer type = (Integer) item.get(0);
             if (type == 0){
+                // ASN1InputStream decoder = new ASN1InputStream((byte[]) item.toArray()[1]);
+                // ASN1Encodable encoded = decoder.readObject();
+                // // encoded = ((DLSequence) encoded).getObjectAt(1);
+                // encoded = ((DLSequence) encoded).getObjectAt(1);
+                // if (encoded instanceof ASN1TaggedObject){
+                //     encoded = ASN1TaggedObject.getInstance((ASN1TaggedObject) encoded, true);
+                //     // encoded = ((ASN1TaggedObject) encoded).getExplicitBaseObject();
+                // }
                 ASN1InputStream decoder = new ASN1InputStream((byte[]) item.toArray()[1]);
                 ASN1Encodable encoded = decoder.readObject();
+                
                 // encoded = ((DLSequence) encoded).getObjectAt(1);
-                encoded = ((DLSequence) encoded).getObjectAt(1);
-                if (encoded instanceof ASN1TaggedObject){
-                    encoded = ASN1TaggedObject.getInstance((ASN1TaggedObject) encoded, true);
-                    // encoded = ((ASN1TaggedObject) encoded).getExplicitBaseObject();
-                }
+                // encoded = ((ASN1TaggedObject) encoded).getExplicitBaseObject();
+                // encoded = ((ASN1TaggedObject) encoded).getExplicitBaseObject();
+
+                encoded = ((DLTaggedObject) encoded).getLoadedObject();
+                // logger.warn("");
+                // encoded = ASN1TaggedObject.getInstance(encoded);
+                // encoded = ASN1TaggedObject.getInstance(encoded);
                 String identity="";
                 if(encoded instanceof DERUTF8String){
                     identity = ((DERUTF8String) encoded).getString();                
@@ -372,6 +384,8 @@ public class CertificateInfo {
                     }
                     logger.warn("DEROctet Decoding is currently unsupported for Subject Alternative Name");
                 } else {
+                   logger.warn("CLASS"+encoded.getClass().getName());
+                   logger.warn("STRANG"+encoded.toString());
                    throw new CertificateParsingException("Invalid Subject Alternative Name");  
                 }
                 identities.add(identity);
